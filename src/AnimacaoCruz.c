ccfe1be63c7ac133c22dfabe23c1b1cad5bf839e
Hilquias R. <hilquiasro10@gmail.com>
2025-01-24 22:03:04 -0300

    Correção dos pinos do teclado

--- src/AnimacaoCruz.c
+++ src/AnimacaoCruz.c
@@ -2,8 +2,8 @@
 #include "pico/stdlib.h"
 #include "AnimacaoCruz.h"
 
-const uint8_t rowPins[] = {12, 13, 14, 15};  // Linhas do teclado
-const uint8_t colsPins[] = {8, 9, 10, 11};  // Colunas do teclado
+const uint8_t rowPins[] = {15, 14, 13, 12};  // Linhas do teclado
+const uint8_t colsPins[] = {11, 10, 9, 8};  // Colunas do teclado
 
 const char keypad[4][4] = {
     {'1', '2', '3', 'A'},
