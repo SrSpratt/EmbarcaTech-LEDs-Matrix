55cbf292967c818801b5a67dbee85386c02c0707
Kaique Rangel <kaiquerangel76@gmail.com>
2025-01-28 09:59:17 -0300

    Salvando mudanças locais antes do pull

--- LEDs_Matrix.c
+++ LEDs_Matrix.c
@@ -4,6 +4,7 @@
 #include <GeneralPinELM.h>
 #include <LEDsELM.h>
 #include "hardware/clocks.h"
+#include "pico/bootrom.h"
 
 int main()
 {
@@ -18,7 +19,7 @@ int main()
 
     // printf("ANTES DE INICIAR\n");
     pio = InitPIO();
-    PrintPIO(pio);
+    //PrintPIO(pio);
 
     // printf("Programa iniciado!\n");
     // printf("Desenho iniciado: \n");
@@ -28,41 +29,102 @@ int main()
     InitKeyboard(ROWS, COLUMNS);
     // printf("DEPOIS DE INICIAR\n");
 
+    SetOutput(BUZZERPIN);
+
     KEYMAP = KeyMap();
 
     char key;
     uint32_t led = 0;
     RGB color[2] = {
-        {.Red = 0.0, .Green = 0.0, .Blue = 0.0}, 
+        {.Red = 0.0, .Green = 1.0, .Blue = 0.0}, 
         {.Red = 1.0, .Green = 1.0, .Blue = 1.0}};
 
-    PrintRGB(color[1]);
+    //PrintRGB(color[1]);
 
     int position[2] = {0,0};
+    Draw(drawing, led, pio, color);
 
     while (true)
     {
         key = ReadMap(KEYMAP, ROWS, COLUMNS);
         if (key == '1')
         {
-            position[0] = 1;
-            position[1] = 3;
-            DrawFrames(drawing, led, pio, color, 1500, position);
+            position[0] = 21;
+            position[1] = 26;
+            DrawFrames(drawing, led, pio, color, 800, position);
         }
-        else if (key == '8')
+        else if (key == '3')
         {
-            color[0].Red = 0.2;
-            color[0].Blue = 0.4;
+            color[0].Red = 0.3;
+            color[0].Blue = 0.9;
+            color[0].Green = 0.3;
+    
+            position[0] = 15;
+            position[1] = 20;
+            DrawFrames(drawing, led, pio, color, 800, position);
+        }
+        else if (key == '4')
+        {
+            color[0].Red = 0.9;
+            color[0].Blue = 0.5;
             color[0].Green = 0.6;
     
-            position[0] = 4;
-            position[1] = 4;
-            DrawFrames(drawing, led, pio, color, 500, position);
+            position[0] = 10;
+            position[1] = 14;
+            DrawFrames(drawing, led, pio, color, 800, position);
         }
-        else
+        else if (key == '5')
+        {
+            color[0].Red = 0.5;
+            color[0].Blue = 0.7;
+            color[0].Green = 0.3;
+    
+            position[0] = 4;
+            position[1] = 9;
+            DrawFrames(drawing, led, pio, color, 800, position);
+        }
+        else if (key == 'A')
+        {
+            RGB none = {.Red = 0.0, .Blue = 0.0, .Green = 0.0};
+            color[0] = none;
+            color[1] = color[0];
             Draw(drawing, led, pio, color);
+        }
+        else if (key == 'B')
+        {
+            RGB blue = {.Red = 0.0, .Blue = 1.0, .Green = 0.0};
+            color[0] = blue;
+            color[1] = color[0];
+            Draw(drawing, led, pio, color);
+        }
+        else if (key == 'C')
+        {
+            RGB red = {.Red = 0.8, .Blue = 0.0, .Green = 0.0};
+            color[0] = red;
+            color[1] = color[0];
+            Draw(drawing, led, pio, color);
+        }
+        else if (key == 'D')
+        {
+            RGB green = {.Red = 0.0, .Blue = 0.0, .Green = 0.5};
+            color[0] = green;
+            color[1] = color[0];
+            Draw(drawing, led, pio, color);
+        }
+        else if (key == '#')
+        {
+            RGB green = {.Red = 0.2, .Blue = 0.2, .Green = 0.2};
+            color[0] = green;
+            color[1] = color[0];
+            Draw(drawing, led, pio, color);
+        }
+        else if (key == '*'){
+            TurnLedsOff(led, pio);
+            break;
+        }
         sleep_ms(100);
     }
-
+    printf("Recolocando em Bootsel!");
+    reset_usb_boot(0,0);
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# EmbarcaTech-LEDs-Matrix
+# EmbarcaTech   
+### TAREFA 2 - U4C2O1234A - V1, Unidade 4 - Capítulo 3
+#### Grupo 4 - Subgrupo 7
+#### Integrantes:
+* Marcel Mascarenhas Andrade
+* Davi Nascimento Leao
+* Hilquias Rodrigues de Oliveira
+* Roberto Vítor Lima Gomes Rodrigues
+
+#### Vídeo de funcionamento
+
+
+#### Instruções de compilação
+Certifique-se de que você tem o ambiente configurado conforme abaixo:
+* Pico SDK instalado e configurado.
+* VSCode com todas as extensões configuradas, CMake e Make instalados.
+* Clone o repositório e abra a pasta do projeto, a extensão Pi Pico criará a pasta build
+* Clique em Compile na barra inferior, do lado direito (ao lado esquerdo de RUN | PICO SDK)
+* Abra o diagram.json
+* Clique em Play
+
+#### Manual do programa
+Ao executar o programa, todos os leds serão ligados com a cor branca.
+A lógica do programa é que ao apertar os botões de números, serão desenhadas animações (em outra cor) a 1,25 frames por segundo (cada animação é mantida por 800 milissegundos). As animações saem cor branca até o desenho principal, que passa 1,6s na tela e depois volta para a cor branca novamente. Cada integrante fez uma animação:
+* Para o número 1, o programa desenha um X em verde. (Roberto)
+* Para o número 3, o programa desenha um quadrado roxo. (Davi)
+* Para o número 4, o programa desenha um W rosa. (Marcel)
+* Para o número 5, o programa desenha uma cruz lilás. (Hilquias)
+    * Obs.: em todas as animações, o buzzer é ligado quando as figuras se iniciam e desligado quando chegam na figura principal (X, quadrado, W, cruz).
+Para as letras:
+* Para a letra A, o programa apaga todas as luzes.
+* Para a letra B, o programa liga todas as luzes na cor azul, com 100% de intensidade.
+* Para a letra C, o programa liga todas as luzes na cor vermelha, com 80% de intensidade.
+* Para a letra D, o programa liga todas as luzes na cor verde, com 50% de intensidade.
+* Para o caracter #, o programa liga todas as luzes na cor branca, com 20% de intensidade.
+    * Obs.: implementamos o quesito opcional do *, que sai do loop e chama a função de pôr a placa em bootsel.
